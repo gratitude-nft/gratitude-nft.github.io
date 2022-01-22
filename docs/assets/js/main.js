109517ef9d6d3a40833489fd3a6c5ba78fdfbb67
Eric Isaiah <40070300+ericisaiahperez@users.noreply.github.com>
2022-01-23 01:05:54 +0800

    Add files via upload

---
+++ docs/assets/js/main.js
@@ -0,0 +1,59 @@
+jQuery( document ).ready(function() {
+
+    // window.onscroll = function() {myFunction()};
+
+    // var header = document.getElementById("site-header");
+    // var sticky = header.offsetTop;
+    
+    // function myFunction() {
+    //   if (window.pageYOffset > sticky) {
+    //     header.classList.add("sticky");
+    //   } else {
+    //     header.classList.remove("sticky");
+    //   }
+    // }
+
+    jQuery('.collection-slider').slick({
+        arrows: false,
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 1500,
+        dots: false,
+        slidesToShow: 6,
+        speed: 500,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            }
+          ]
+    });
+
+    jQuery('a').click(function(){
+        jQuery('html, body').animate({
+            scrollTop: jQuery( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+
+    jQuery('#gnft-provenance table .ipfs-hash-provenance a').text(function(index, text) { 
+      return text.replace('https://ipfs.io/ipfs/', ''); 
+    });
+
+    console.clear();
+
+    AOS.init();
+
+
+});
+
